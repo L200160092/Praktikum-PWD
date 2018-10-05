0eb4aec9985190311c3af9883565b40ae5a0e464
Yoga Widi Sejati <43274901+L200160092@users.noreply.github.com>
2018-10-05 20:46:34 +0700

    Add files via upload

---
+++ Modul3/Tugasmodul3.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+		<title>Program Penjumlahan</title>
+	</head>
+	
+	<body>
+		<form method="POST" action="Tugasmodul3.php">
+			Nilai A adalah : <input type="text" name="a" >
+			<br>Nilai B adalah : <input type="text" name="b" >
+			<br><input type="submit" name="c" value="jumlahkan">
+		</form>
+		<?php
+		error_reporting (E_ALL ^ E_NOTICE);
+		$A=$_POST['a'];
+		$B=$_POST['b'];
+		$submit=$_POST['c'];
+		$jumlah= $A + $B ;
+		 if($submit){
+			 echo "nilai A adalah $A<br>";
+			 echo "nilai B adalah $B<br>";
+			 echo " hasil penjumlahan daei $A dan $B adalah $jumlah";
+			 
+		 }
+		?>
+	</body>
+</html>
